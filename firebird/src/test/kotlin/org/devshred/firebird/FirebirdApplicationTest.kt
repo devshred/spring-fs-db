8036e3faa1ec808d9d67cef7e8cdb71a930be6bd
Johannes Schmidt <johannes.schmidt@codecentric.de>
2021-12-12 16:44:59 +0100

    disable firebird tests

--- firebird/src/test/kotlin/org/devshred/firebird/FirebirdApplicationTest.kt
+++ firebird/src/test/kotlin/org/devshred/firebird/FirebirdApplicationTest.kt
@@ -1,9 +1,11 @@
 package org.devshred.firebird
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@Disabled("disabled since Firebird is not running on every platform")
 class FirebirdApplicationTest {
     @Test
     fun contextLoads() {
